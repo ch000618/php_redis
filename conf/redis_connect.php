8c82e8c44b83a2cdfc73ea5bc65d8be0d1bfc936
ch000618 <df9904201@gmail.com>
2017-10-16 16:58:24 +0800

    update class.redis 2
    debug

--- conf/redis_connect.php
+++ conf/redis_connect.php
@@ -4,7 +4,7 @@ $redis_set=array(
 	'host'=>'192.168.1.190'
 	,'port'=>6379
 	,'db_rds'=>0
-	,'rds_enabled'=>1
+	,'rds_enabled'=>0
 	,'db_mysql'=>'test'
 	,'pass'=>'1234'
 );
--- lib/class.redis.php
+++ lib/class.redis.php
@@ -115,6 +115,9 @@ class php_redis{
 		if(!class_exists('Redis')){
 			return $this->status_code;
 		}
+		if($this->redis_enabled==0){
+			return $this->status_code;
+		}
 		$this->pass=$pass;
 		$r=$this -> link -> auth($this->pass);
 		if($r) {
